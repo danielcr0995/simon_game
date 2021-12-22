9396843c930ca10ea702b86ca8cd96bd185ccd5d
danielcr0995 <daniel.correa.1995@gmail.com>
2021-12-22 16:10:33 -0500

    add a click pattern to an array

--- simon.js
+++ simon.js
@@ -1,11 +1,42 @@
 var gamePattern = [];
-var buttonColours=["red", "blue", "green", "yellow"];
-
-var randomChosenColour=buttonColours[nextSequence()];
-
-gamePattern.push(randomChosenColour);
+var userClickedPattern=[];
+var buttonColors=["red", "blue", "green", "yellow"];
 
 function nextSequence(){
     var randomNumber=Math.floor(Math.random()*4);
-    return randomNumber;
+    
+    var randomChosenColor=buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColor);
+
+    $('#'+randomChosenColor).fadeOut(100).fadeIn(100);
+
+    var fileName='./sounds/'+randomChosenColor+'.mp3';
+    var sound = new Audio(fileName);
+    sound.play();
+
 }
+
+var userChosenColor=$('.btn').click(colorChosen)
+
+
+
+function colorChosen(event){
+    var color=this.id;
+    userClickedPattern.push(color);
+
+}
+
+function playSound(name){
+    var fileName='./sounds/'+name+'.mp3';
+    var sound = new Audio(fileName);
+    sound.play();
+}
+
+function colorAudio(){
+    var fileName='./sounds/'+randomChosenColor+'.mp3';
+    var colorSound=new Audio(fileName);
+    colorSound.play();
+}
+
+
